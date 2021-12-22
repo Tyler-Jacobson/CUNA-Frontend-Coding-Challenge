89810305d04283f571d2e0892e84327228d6ef40
Tyler Jacobson <TylerIRL2@gmail.com>
2021-12-21 19:14:42 -0800

    Set up forms and local form state

--- src/Components/Signup.tsx
+++ src/Components/Signup.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+
+const defaultFormValues = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+}
+
 
 export default function Signup() {
+
+    const [ formValues, setFormValues ] = useState(defaultFormValues)
+
+    function onChange(e: FormEvent<HTMLInputElement>) {
+        setFormValues({
+            ...formValues,
+            [e.currentTarget.name]: e.currentTarget.valueAsNumber || e.currentTarget.value
+        })
+    }
+
+
     return (
         <div>
             <h1>Sign Up Here</h1>
+            <form>
+                <label>
+                    Email:
+                    <input name="username" type="text" onChange={onChange} value={formValues.username}/> 
+                </label>
+                
+                <label>
+                    Password:
+                    <input name="password" type="text" onChange={onChange} value={formValues.password}/> 
+                </label>
+
+                <label>
+                    Confirm Password:
+                    <input name="confirmPassword" type="text" onChange={onChange} value={formValues.confirmPassword}/> 
+                </label>
+            </form>
         </div>
     )
 }
