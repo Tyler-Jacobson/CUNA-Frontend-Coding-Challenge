0f29f73f21b23af58c663b60206d02829919f864
Tyler Jacobson <TylerIRL2@gmail.com>
2021-12-28 00:38:07 -0800

    Created tests for landing page

--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import { createStore, compose } from 'redux'
 import { Provider } from "react-redux"
-import rootReducer from "./reducers"
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers());
+import store from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store()}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
@@ -29,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
